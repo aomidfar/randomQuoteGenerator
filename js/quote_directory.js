49bb3ada457688aa8d23d2ec7bfa4f986299752f
aomidfar <82061812+aomidfar@users.noreply.github.com>
2021-04-27 19:24:06 -0700

    Update quote_directory.js
    I have removed the 'undefined' strings out of the objects

--- js/quote_directory.js
+++ js/quote_directory.js
@@ -4,31 +4,22 @@ const quotes = [  {
     quote: "Life is too short to be living somebody else's dream.",
     source: "Hugh Hefner",
     citation: "elite daily",
-    year: ""
   },
   { quote: "The man who has confidence in himself gains the confidence of others.",
     source: "Hasidic Proverb",
     citation: "www.quotespedia.org",
-    year :""
   },
   { quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
     source: "Mahatma Gandhi",
-    citation: "",
-    year :""
   },
   { quote: "Do it with passion, or not at all.",
   source: "Rosa Nouchette Carey",
-  citation: "",
-  year :""
   },
   { quote: "The grass is greener where you water it.",
   source: "Neil Barringham",
-  citation: "",
-  year :""
   },
   { quote: "Democracy is being allowed to vote for the candidate you dislike least.",
   source: "Robert Byrne",
-  citation: "",
   year :"1976",
   tag: "political"
   }]
