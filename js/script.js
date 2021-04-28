1820bc72895560f5b64a19f27c44a1dbf69ca448
aomidfar <82061812+aomidfar@users.noreply.github.com>
2021-04-27 19:19:30 -0700

    Update script.js
    The previous version only changed the background picture when page was refreshed and not when the quotes changed by the button and I have fixed it by creating a getRandomColor function that gets called every time new quote is generated. There was also an issue with the previous version where it would check the entire array to see if it has citation..etc and not the random one that has been generated from the main array. I have fixed that and now it checks the random picked array to see what properties it has and if it does have one it will display it including the tag.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator by Ali Omidfar
 ******************************************/
+// picking a few background color choices and storing it in colorChoices
 
-
+let colorChoices = [
+  'green',
+  'blue',
+  'brown',
+  'black',
+  'yellow',
+  'orange'
+  ] 
+  getRandomColor ();
 /***
  * `getRandomQuote` function generates a random number not higher than the length of the objects 
  * it then puts that number object into randomQuote and returns it
@@ -21,46 +30,52 @@ function getRandomQuote (){
  //document.querySelector('main').innerHTML = getRandomQuote(); 
 
 function printQuote() {
+  
   let html = '';
   let randomQuote = getRandomQuote();
-   html +=`<h2>  ${randomQuote.quote} </h2> 
-  <br><h3>    _${randomQuote.source},</h3>`;
+   html +=`<p class="quote">  ${randomQuote.quote} </p> 
+   <p class="source"> ${randomQuote.source}</p>`;
   
-  if (quotes.citation !== 'undefined'){
+  if (randomQuote.citation){
     
    html += `
-    <br>
-    <h3>    ${randomQuote.citation}</h3> `;}
+    <span class="citation">   ${randomQuote.citation}</span> `;}
+
+    if (randomQuote.tag){
     
-    if (quotes.year !== 'undefined'){
-      html += ` <h3>    ${randomQuote.year}</h3> `; }; 
-   return document.getElementById('quote-box').innerHTML = html;
+      html += `
+       <span class="tag">   ${randomQuote.tag}</span> `;}
+    
+    if (randomQuote.year){
+      html += ` <span class="year">    ${randomQuote.year}</span> `; }; 
+      getRandomColor ();
+      return document.getElementById('quote-box').innerHTML = html;
 };
 
 
 
-//document.querySelector('main').innerHTML = printQuote();
 document.getElementById('quote-box').innerHTML = printQuote();
 
-// picking a few color choices and storing it in colorChoices
-let colorChoices = [
-'green',
-'blue',
-'brown',
-'black',
-'yellow',
-'orange'
-]
 
 // using random number code (up to length of the colors) to pick that number color and put on background
-let randomColor = colorChoices[Math.floor( Math.random() * colorChoices.length )];
-document.body.style.backgroundColor = randomColor;
+
+function getRandomColor () {
+  let randomColor = colorChoices[Math.floor( Math.random() * colorChoices.length )];
+  document.body.style.backgroundColor = randomColor; 
+  return randomColor;
+}
+
 
 // auto refresh function every 10 seconds
 setInterval(function(){
-  window.location.reload(1);
+ window.location.reload(1);
 }, 10000);
 
+//printQuote();
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
 
 
  document.getElementById('load-quote').addEventListener("click", printQuote, false);
